d55970eb6b47b401800acb2dd953432f641e6514
shermy0 <shermydihya98@gmail.com>
2025-10-10 13:33:16 +0700

    migration

---
+++ database/migrations/2025_10_10_063159_create_ulasan_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ulasan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pemesanan_id')->constrained('pemesanan')->onDelete('cascade');
+            $table->foreignId('penyewa_id')->constrained('users')->onDelete('cascade');
+            $table->tinyInteger('rating')->check('rating >= 1 and rating <= 5');
+            $table->text('komentar')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ulasan');
+    }
+};
