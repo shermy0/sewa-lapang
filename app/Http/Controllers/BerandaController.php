d58407fcb8a0ae3613b5dbbb54ad4af52f26904a
starfollowme <irhammuhamad124@gmail.com>
2025-10-14 13:41:39 +0700

    ui verify

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -11,11 +11,19 @@ class BerandaController extends Controller
     {
         $keyword = $request->input('search');
         $kategori = $request->input('kategori');
+        $kategoris = [];
 
         // Ambil semua nilai enum kategori langsung dari struktur tabel
         $enumQuery = DB::select("SHOW COLUMNS FROM lapangan LIKE 'kategori'");
-        preg_match("/^enum\('(.*)'\)$/", $enumQuery[0]->Type, $matches);
-        $kategoris = explode("','", $matches[1]);
+
+        if (!empty($enumQuery) && isset($enumQuery[0]->Type)) {
+            // Cocokkan pola enum('...') secara fleksibel
+            if (preg_match("/^enum\((.*)\)$/i", $enumQuery[0]->Type, $matches)) {
+                // Hilangkan tanda kutip tunggal dan spasi berlebih
+                $raw = str_replace("'", "", $matches[1]);
+                $kategoris = array_map('trim', explode(',', $raw));
+            }
+        }
 
         // Query data lapangan
         $lapangan = DB::table('lapangan')
@@ -30,15 +38,16 @@ class BerandaController extends Controller
 
         return view('penyewa.beranda', compact('lapangan', 'keyword', 'kategori', 'kategoris'));
     }
-    
+
     public function detail($id)
     {
-        $lapangan = \DB::table('lapangan')->where('id', $id)->first();
-        $lainnya = \DB::table('lapangan')
+        $lapangan = DB::table('lapangan')->where('id', $id)->first();
+
+        $lainnya = DB::table('lapangan')
             ->where('id', '!=', $id)
             ->limit(4)
             ->get();
 
         return view('penyewa.detail', compact('lapangan', 'lainnya'));
-    }     
+    }
 }
