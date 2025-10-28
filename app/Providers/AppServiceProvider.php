8fd8603c831b7a7b71c028a8fe04ea3fea6f7403
shermy0 <shermydihya98@gmail.com>
2025-10-28 11:56:41 +0700

    ..

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace App\Providers;
+use Midtrans\Config;
 
 use Illuminate\Support\ServiceProvider;
-use Midtrans\Config as MidtransConfig; // <── ini penting!
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,11 +18,12 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+
     public function boot(): void
     {
-        MidtransConfig::$serverKey = env('MIDTRANS_SERVER_KEY');
-        MidtransConfig::$isProduction = env('MIDTRANS_IS_PRODUCTION', false);
-        MidtransConfig::$isSanitized = true;
-        MidtransConfig::$is3ds = true;
+        Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+        Config::$isProduction = env('MIDTRANS_IS_PRODUCTION', false);
+        Config::$isSanitized = true;
+        Config::$is3ds = true;
     }
 }
