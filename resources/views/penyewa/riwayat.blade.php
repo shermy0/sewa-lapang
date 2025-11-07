9347cb6ffe43edbf6ba4c3720562f24776ee014d
bayuresnadi11 <bayuresnadi700@gmail.com>
2025-11-07 14:46:38 +0700

    i

--- resources/views/penyewa/riwayat.blade.php
+++ resources/views/penyewa/riwayat.blade.php
@@ -3,13 +3,15 @@
 @section('title', 'Riwayat Pemesanan')
 
 @section('content')
+<link rel="stylesheet" href="{{ asset('css/riwayat.css') }}">
+
 <div class="container py-4">
-    <h2 class="fw-bold mb-4 text-secondary">Riwayat Pemesanan</h2>
+    <h2 class="fw-bold mb-4">Riwayat Pemesanan</h2>
 
     <div class="row">
         @forelse($dibatalkan as $p)
         <div class="col-md-4 mb-3">
-            <div class="card p-3 border-secondary">
+            <div class="card p-3">
                 <p><strong>Lapangan:</strong> {{ $p->lapangan->nama_lapangan }}</p>
                 <p><strong>Jadwal:</strong> 
                     {{ \Carbon\Carbon::parse($p->jadwal->tanggal)->format('d M Y') }}
