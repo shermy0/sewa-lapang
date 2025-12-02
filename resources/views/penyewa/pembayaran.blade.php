fa22ed3092a7bf037aa9cbe12c06109720d6ccbb
starfollowme <irhammuhamad124@gmail.com>
2025-12-02 11:21:55 +0700

    perbaikan refresh tiket

--- resources/views/penyewa/pembayaran.blade.php
+++ resources/views/penyewa/pembayaran.blade.php
@@ -221,7 +221,9 @@ document.querySelectorAll('.btn-pay-again').forEach(btn => {
                                 'Content-Type': 'application/json'
                             },
                             body: JSON.stringify({ result })
-                        }).then(() => window.location.reload());
+                        }).then(() => {
+                            window.location.href = "{{ route('penyewa.tiket') }}";
+                        });
                     },
                     onPending: function(){
                         Swal.fire('Menunggu Pembayaran', 'Silakan selesaikan pembayaranmu.', 'info')
